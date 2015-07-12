53a78bece8634e09b89cd80da72eeeccd27069e9
Ahmed Awad <b00042183@aus.edu>
2015-07-12 23:56:09 +0400

    Fixed scrolling issue

--- emberblog/urls.py
+++ emberblog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^$', 'blog.views.index', name='home'),
     url(r'^blogs/$', 'blog.views.index'),
     url(r'^about/$', 'blog.views.index'),
-    url(r'^blogs/*$', 'blog.views.index'),
+    url(r'^blogs/', 'blog.views.index'),
     url(r'^api/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
