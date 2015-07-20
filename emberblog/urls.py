504f4fac21adb6815c2e98973c5e51e66236b294
Ahmed Awad <b00042183@aus.edu>
2015-07-21 01:45:11 +0400

    Fixed rest-auth urls

--- emberblog/urls.py
+++ emberblog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^blogs/', 'blog.views.index'),
     url(r'^api/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
 )
