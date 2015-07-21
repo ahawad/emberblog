a2e26a876f60dc98b86a8c8a026755962ec58bf1
Ahmed Awad <b00042183@aus.edu>
2015-07-21 22:52:51 +0400

    Added login url

--- emberblog/urls.py
+++ emberblog/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'blog.views.index', name='home'),
     url(r'^blogs/$', 'blog.views.index'),
+    url(r'^login/$', 'blog.views.index'),
     url(r'^about/$', 'blog.views.index'),
     url(r'^blogs/', 'blog.views.index'),
     url(r'^api/', include('blog.urls')),
